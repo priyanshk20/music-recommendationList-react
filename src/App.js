7b09ffbce2d4b7ac9093645700a53425326dfea3
Priyansh Khandelwal <52736997+priyanshk20@users.noreply.github.com>
2021-06-08 00:49:55 +0530

    app done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,89 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+
+const songDB = {
+  pop: [
+    { name: "One Dance", rating: "4/5" },
+    { name: "There For You", rating: "3.5/5" }
+  ],
+
+  rap: [
+    {
+      name: "Slim Shady",
+      rating: "5/5"
+    },
+    {
+      name: "3:59",
+      rating: "4.5/5"
+    }
+  ],
+  indie: [
+    {
+      name: "Liggi",
+      rating: "3.5/5"
+    },
+    {
+      name: "Sage",
+      rating: "5/5"
+    }
+  ]
+};
+
 export default function App() {
-
-
+  const [selectedGenre, setGenre] = useState("pop");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
   return (
     <div className="App">
       <h1>WhatMusic</h1>
-      <p style = {{fontSize:"larger"}}>My Recommended Music List is Here, select a Genre to get started.</p>
-      <button style = {{padding:"0.5rem", width:"6rem", margin:"1rem", borderRadius:"0.5rem", border:"1px solid black", cursor:"pointer"}} >Pop</button>
-       <button style = {{padding:"0.5rem", width:"6rem", margin:"1rem", borderRadius:"0.5rem", border:"1px solid black", cursor:"pointer"}} >Rap</button>
-        <button style = {{padding:"0.5rem", width:"6rem", margin:"1rem", borderRadius:"0.5rem", border:"1px solid black", cursor:"pointer"}} >Indie</button>
-        <hr  style={{
-    color: '#000000',
-    backgroundColor: '#000000',
-    height: .5,
-    borderColor : '#000000'
-}}/>
+      <p style={{ fontSize: "smaller" }}>
+        {" "}
+        Checkout my favorite books. Select a genre to get started{" "}
+      </p>
+
+      <div>
+        {Object.keys(songDB).map((genre) => (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            style={{
+              cursor: "pointer",
+              background: "#E5E7EB",
+              borderRadius: "0.5rem",
+              padding: "0.5rem  1rem",
+              border: "1px solid black",
+              margin: "1rem 0.3rem"
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {songDB[selectedGenre].map((book) => (
+            <li
+              key={book.name}
+              style={{
+                listStyle: "none",
+                padding: "1rem",
+                border: "1px solid #D1D5DB",
+                width: "70%",
+                margin: "1rem 0rem",
+                borderRadius: "0.5rem"
+              }}
+            >
+              {" "}
+              <div style={{ fontSize: "larger" }}> {book.name} </div>
+              <div style={{ fontSize: "smaller" }}> {book.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
+
+
